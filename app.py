651fd9d49e0910975af4e572cb3b0cb238a6ab32
Ian Swift & Maxime Goovaerts <pair+iswift+max@pivotallabs.com>
2017-01-03 21:02:43 -0500

    Basic arithmatic

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import urllib
 import json
 import os
+import math
 
 from flask import Flask
 from flask import request
@@ -10,7 +11,6 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -27,62 +27,50 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
     result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
+    if result.get("metadata").get("intentName") == "cummulate-expression":
+        params = result.get("parameters")
+        n1 = params.get("number1")
+        n2 = params.get("number2")
+        return options[params.get("operation")](float(n1) if n1 else 0.0, float(n2) if n2 else 0.0)
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+def plus(n1, n2):
+    return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+def squared(n1, n2):
+    return n1 * n1
+
+def halved(n1, n2):
+    return n1 / 2
+
+def doubled(n1, n2):
+    return n1 * 2
+
+def sqrt(n1, n2):
+    return math.sqrt(n1)
+
+options = {
+    'plus' : plus,
+    'minus': subtract,
+    'mul': mul,
+    'divide': divide,
+    'square': squared,
+    'halve': halved,
+    'doubled': doubled,
+    'sqrt': sqrt
+}
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
-
     return {
         "speech": speech,
         "displayText": speech,
@@ -90,7 +78,6 @@ def makeWebhookResult(data):
         # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
-
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
